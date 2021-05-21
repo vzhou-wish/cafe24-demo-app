972d4c1aee9d8ba2bdd41b5015a1801e32fd733c
vzhou-wish <vzhou@wish.com>
2021-05-20 23:27:22 -0400

    modify client id

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //hard-coded clientId
-const clientId = "zr4BCdJJKxtiHI0lkoO3FA"
+const clientId = "vKRKfJADOKRJT5nAM4qEDC"
 
 window.addEventListener("load",function(event) {
     (function(CAFE24API) {
         $("#mallid-display").html(`<h2>Your mall id is: ${CAFE24API.MALL_ID}</h2>`)
         })(CAFE24API.init(clientId));
- },false);
+ },false)
