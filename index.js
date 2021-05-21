7664c4d0e741548e5d543dd8b0223c7c35b3a3f4
vzhou-wish <vzhou@wish.com>
2021-05-21 13:51:06 -0400

    experiment with fetch, admin API

--- index.js
+++ index.js
@@ -3,17 +3,20 @@ const CLIENT_ID = "vKRKfJADOKRJT5nAM4qEDC"
 //const ACCESS_TOKEN = "O561i3AlHBnrXLeGo8c4sC"
 
 window.addEventListener("load", (event)=>{
-    fetch(`https://${MALL_ID}.cafe24api.com/api/v2/products`, {
+    fetch(`https://${MALL_ID}.cafe24api.com/api/v2/admin/categories/40`, {
         mode: "no-cors",
         method:"GET",
         headers: {
             "Content-Type": "application/json",
-            "X-Cafe24-Client-Id": `${CLIENT_ID}`
+            "Authorization": `Bearer ${ACCESS_TOKEN}`
         }
     })
     .then(res=>res.json())
     .then(data=>{
         console.log(data);
         $("#mallid-display").html(`<h2>${data}</h2>`)
+    })
+    .catch(err=>{
+        console.log(err)
     })
 })
