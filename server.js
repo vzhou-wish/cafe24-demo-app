670a44d3b73271d915bb0e8f06866c5b509fedb2
vzhou-wish <vzhou@wish.com>
2021-05-21 22:18:06 -0400

    node server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const path = require("path")
 const http = require("http")
 const cors = require("cors")
-const port = 3000
+const port = process.env.PORT || 3000
 
 app = express()
 
--- static/index.js
+++ static/index.js
@@ -1,8 +1,23 @@
-//hard-coded clientId
-const clientId = "zr4BCdJJKxtiHI0lkoO3FA"
+require("dot-env").config()
 
-window.addEventListener("load",function(event) {
-    (function(CAFE24API) {
-        $("#mallid-display").html(`<h2>Your mall id is: ${CAFE24API.MALL_ID}</h2>`)
-        })(CAFE24API.init(clientId));
- },false);
+const MALL_ID = process.env.MALL_ID
+const CLIENT_ID = process.env.CLIENT_ID
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN
+
+window.addEventListener("load", (event)=>{
+    fetch(`https://${MALL_ID}.cafe24api.com/api/v2/admin/categories/40`, {
+        method:"GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${ACCESS_TOKEN}`
+        }
+    })
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data);
+        $("#mallid-display").html(`<h2>${data}</h2>`)
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
